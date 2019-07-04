30aa3e2ebc53959dbcb6641720a24918b64af5dc
mslichao <mslichao@outlook.com>
2019-07-04 10:57:38 +0000

    multi

--- pytorch/mytrain.py
+++ pytorch/mytrain.py
@@ -1,14 +1,10 @@
 import subprocess
 import os
 
-os.system('python main.py --mode=train')
-os.system('python main.py --mode=train --reloadid=0')
-os.system('python main.py --mode=train --reloadid=1')
-os.system('python main.py --mode=train --reloadid=2')
-os.system('python main.py --mode=train --reloadid=3')
-os.system('python main.py --mode=train --reloadid=4')
-os.system('python main.py --mode=train --reloadid=5')
-os.system('python main.py --mode=train --reloadid=6')
-os.system('python main.py --mode=train --reloadid=7')
+for i in range(11, 2000):
+    cmd=str.format('python main.py --mode=train --reloadid=%d'%(i))
+    ret=os.system(cmd)
+    if ret != 0:
+        break
 
 
